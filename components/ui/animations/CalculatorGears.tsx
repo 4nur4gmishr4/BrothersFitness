0c293106c81d6603458534aed2201c48543473ab
Anurag Mishra <anuragmishrasnag06082004@gmail.com>
2026-09-02 22:17:14 +0530

    remove dead animation components orphan audio assets and unused package dependencies

--- components/ui/animations/CalculatorGears.tsx
+++
@@ -1,9 +0,0 @@
-import AnimatedSvgIcon from "@/components/ui/AnimatedSvgIcon";
-
-export default function CalculatorGears({ size = 48 }: { size?: number }) {
-  return (
-    <div style={{ width: size, height: size }}>
-      <AnimatedSvgIcon src="/animatedsvgs/lottie_gears.svg" className="w-full h-full" themeColor="accent" />
-    </div>
-  );
-}
